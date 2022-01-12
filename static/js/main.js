49bf26d3e11480b6ca6176ab688ec0593c8288a8
seunghwan13 <childwelfare23@gmail.com>
2022-01-12 20:33:28 +0900

    main.js index.html 주석 추가

--- static/js/main.js
+++ static/js/main.js
@@ -15,26 +15,26 @@ function readURL(input) {
 function startTimer(duration, display, bar) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
+        minutes = parseInt(timer / 60, 10); //10진수 표현을 위해 radix를 10으로 합니다.(만약 2이면 2진수)
         seconds = parseInt(timer % 60, 10);
 
         var totalSeconds = 1 * 60
         , remainingSeconds = minutes * 60 + seconds
 
-        bar.style.width = (remainingSeconds*100/totalSeconds) + "%";
+        bar.style.width = (remainingSeconds*100/totalSeconds) + "%"; //
 
         minutes = minutes < 10 ? "0" + minutes : minutes; // ? 조건문입니다. 10분 아래로 떨어지면 09:30 이렇게 9분 앞에 0을 붙입니다. 방식으로 표현합니다.
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         display.textContent = minutes + ":" + seconds; // 화면 표시 분과 초를 :로 구분합니다.
         // if 조건문입니다. --timer: timer가 2일 때 --timer는 1을 할당하고 1을 반환합니다.
-        // timeout 이 1000일 때는 1초 10000일 때는 10초의 지연시간이 있고 다시 실행을 반복합니다.
+        // timeout 이 1000일 때는 1초 10000일 때는 10초의 지연시간이 있고 다시 실행을 반복합니다.(위의 setInterval 메서드)
         if (--timer < 0) {
             timer = duration;
         }
     }, 1000);
 }
-
+// document.querySelector는 CSS의 클래스, 아이디를 찾아 같은 값을 반환합니다.
 window.onload = function () {
     var minutes = 60 * 1,
     display = document.querySelector('#time');
