d27bb86027fcfab5947d7bbe2843e6b1f34f919c
bolonge <qhsghd5357@gamil.com>
2022-01-12 19:51:38 +0900

    프리뷰 기능

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,13 @@
-$("#img-input").on('change', function () {
-    readURL(this);
+$(document).on("input", "input:file", function() {
+        readURL(this)
 });
 
 function readURL(input) {
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
+        let reader = new FileReader();
         reader.onload = function (e) {
             $('#user-img').attr('src', e.target.result);
         }
-
         reader.readAsDataURL(input.files[0]);
     }
 }
