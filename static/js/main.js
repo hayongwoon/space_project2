b3d143b0a7f9a836a1c8a08ce70b23b5ada05282
bolonge <qhsghd5357@gamil.com>
2022-01-12 22:29:21 +0900

    타임아웃 시 알람alert

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
-$(document).on("input", "input:file", function() {
-        readURL(this)
+$(document).on("input", "input:file", function () {
+    readURL(this)
 });
 
 function readURL(input) {
@@ -11,17 +11,18 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
 //parseInt는 문자열 인자를 파싱을 통해 특정 정수로 표현할 수 있습니다.
 function startTimer(duration, display, bar) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
+    let timer = duration, minutes, seconds;
+    const time_set = setInterval(function () {
         minutes = parseInt(timer / 60, 10); //10진수 표현을 위해 radix를 10으로 합니다.(만약 2이면 2진수)
         seconds = parseInt(timer % 60, 10);
 
-        var totalSeconds = 1 * 60
-        , remainingSeconds = minutes * 60 + seconds
+        const totalSeconds = 5
+            , remainingSeconds = minutes * 60 + seconds
 
-        bar.style.width = (remainingSeconds*100/totalSeconds) + "%"; //
+        bar.style.width = (remainingSeconds * 100 / totalSeconds) + "%"; //
 
         minutes = minutes < 10 ? "0" + minutes : minutes; // ? 조건문입니다. 10분 아래로 떨어지면 09:30 이렇게 9분 앞에 0을 붙입니다. 방식으로 표현합니다.
         seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -30,15 +31,21 @@ function startTimer(duration, display, bar) {
         // if 조건문입니다. --timer: timer가 2일 때 --timer는 1을 할당하고 1을 반환합니다.
         // timeout 이 1000일 때는 1초 10000일 때는 10초의 지연시간이 있고 다시 실행을 반복합니다.(위의 setInterval 메서드)
         if (--timer < 0) {
-            timer = duration;
+            showAlert()
+            clearInterval(time_set)
         }
     }, 1000);
 }
+
+function showAlert(){
+    document.getElementById('alert-container').classList.replace('hide','show')
+}
+
 // document.querySelector는 CSS의 클래스, 아이디를 찾아 같은 값을 반환합니다.
 window.onload = function () {
-    var minutes = 60 * 1,
-    display = document.querySelector('#time');
-    bar = document.querySelector('#progressBar');
+    let minutes = 5,
+        display = document.querySelector('#time');
+    let bar = document.querySelector('#progressBar');
     startTimer(minutes, display, bar);
 };
 
