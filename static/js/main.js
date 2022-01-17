e31a9e872bbe0378466efbaf370cfb4d97ebde4e
하용운 <a01046641857@gmail.com>
2022-01-17 16:30:46 +0900

    -추가 타이머 기능 추가

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,6 @@ $(document).on("input", "input:file", function () {
     document.getElementById('user-img').classList.replace('invisible','visible')
 });
 let news_modal = true
-var sound = new Audio("https://raw.githubusercontent.com/Xiija/TestFiles/master/Yuki%20%26%20Tako%2001.mp3");
 
 function readURL(input) {
     if (input.files && input.files[0]) {
@@ -14,39 +13,6 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
-
-/*//parseInt는 문자열 인자를 파싱을 통해 특정 정수로 표현할 수 있습니다.
-function startTimer(duration, display, bar) {
-    let timer = duration, minutes, seconds;
-    const time_set = setInterval(function () {
-        minutes = parseInt(timer / 60, 10); //10진수 표현을 위해 radix를 10으로 합니다.(만약 2이면 2진수)
-        seconds = parseInt(timer % 60, 10);
-
-        const totalSeconds = 5
-            , remainingSeconds = minutes * 60 + seconds
-
-        bar.style.width = (remainingSeconds * 100 / totalSeconds) + "%"; //
-
-        minutes = minutes < 10 ? "0" + minutes : minutes; // ? 조건문입니다. 10분 아래로 떨어지면 09:30 이렇게 9분 앞에 0을 붙입니다. 방식으로 표현합니다.
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds; // 화면 표시 분과 초를 :로 구분합니다.
-        // if 조건문입니다. --timer: timer가 2일 때 --timer는 1을 할당하고 1을 반환합니다.
-        // timeout 이 1000일 때는 1초 10000일 때는 10초의 지연시간이 있고 다시 실행을 반복합니다.(위의 setInterval 메서드)
-        if (--timer < 0) {
-            showAlert()
-            clearInterval(time_set)
-            sound.play();
-        }
-    }, 1000);
-}
-// document.querySelector는 CSS의 클래스, 아이디를 찾아 같은 값을 반환합니다.
-window.onload = function () {
-    let minutes = 5,
-        display = document.querySelector('#time');
-    let bar = document.querySelector('#progressBar');
-    startTimer(minutes, display, bar);
-};*/
 
 
 function showAlert(){
@@ -73,30 +39,6 @@ function able_button() {
     document.getElementById('check-button').disabled = false;
     document.getElementById('file-input').disabled = false;
 }
-//
-// function checkImage(){
-//     var prerandomYogaImg = document.getElementById('yoga-img').src.split('/');
-//     var randomYogaImg = prerandomYogaImg[5].split(".")[0];
-//     console.log(randomYogaImg)
-//
-//     $.ajax({
-//         type: "GET",
-//         url: "/fileupload",
-//         contentType: "application/json; charset=utf-8",
-//         data: {},
-//         success: function (response){
-//             alert(response);
-//         }
-//     });
-function checkImage(){
-    var prerandomYogaImg = document.getElementById('yoga-img').src.split('/');
-    var randomYogaImg = prerandomYogaImg[5].split(".")[0];
-
-
-    // alert(yoga_img)
-    alert(randomYogaImg);
-
-}
 
 function posting() {
     let file = $('#file-input')[0].files[0]
@@ -114,10 +56,9 @@ function posting() {
         contentType: false,
         processData: false,
         success: function (response) {
-            alert(response["result"])
-            alert(randomYogaImg)
             if (response["result"] == randomYogaImg){
                 alert('coreect');
+                alarmClear();
                 sound.pause();
                 $("#button-container").addClass('invisible');
                 $("#success-container").attr('class', 'visible');
--- static/js/timer.js
+++ static/js/timer.js
@@ -1,86 +1,164 @@
-// element 안에 minutes, seconds, control, reset을 queryselector 사용하여 index.html에서 불러옵니다.
-el = {
-    minutes: document.querySelector(".timer_minutes"),
-    seconds: document.querySelector(".timer_seconds"),
-    control: document.querySelector(".timer_btn_control"),
-    reset: document.querySelector(".timer_btn_reset"),
-};
-// 변수 재할당이 가능한 let을 사용하여 interval과 remainingSeconds을 선언합니다.
-let interval = null;
-let remainingSeconds = 0;
-// event 사용이 가능한 addEventListener을 사용합니다. 클릭 이벤트 발생시 조건을 줍니다.
-el.control.addEventListener("click", () =>{
-    if (interval === null) {
-        start();
+var sound = new Audio("https://raw.githubusercontent.com/Xiija/TestFiles/master/Yuki%20%26%20Tako%2001.mp3");
+sound.loop = true;
+
+var h2 = document.getElementById('clock');
+
+// display current time by the second
+var currentTime = setInterval(function () {
+    var date = new Date();
+
+    var hours = (12 - (date.getHours()));
+    // var hours = date.getHours();
+
+    var minutes = date.getMinutes();
+
+    var seconds = date.getSeconds();
+
+    var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+    //convert military time to standard time
+
+    if (hours < 0) {
+        hours = hours * -1;
+    } else if (hours == 0) {
+        hours = 12;
     } else {
-        stop();
+        hours = hours;
     }
-});
-// reset 버튼 클릭 이벤트입니다. 분은 60분 미만으로 작성가능합니다. 아래 updatetime 함수로 그 값을 보냅니다.
-el.reset.addEventListener("click", () =>{
-    const inputMinutes = prompt("몇 분으로 타이머 맞출거냐? 60미만 숫자만 적어라~ ex)40분이면 40을 입력");
 
-    if (inputMinutes < 60) {
-        stop();
-        remainingSeconds = inputMinutes * 60;
-        updateTime();
+
+    h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+
+}, 1000);
+
+
+/*functions to get hour, min, secs,
+  am or pm, add zero, set alarm time and sound, clear alarm
+*/
+
+function addZero(time) {
+
+    return (time < 10) ? "0" + time : time;
+
+}
+
+function hoursMenu() {
+
+    var select = document.getElementById('alarmhrs');
+    var hrs = 12
+
+    for (i = 1; i <= hrs; i++) {
+        select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+
     }
-    else if (inputMinutes >= 60) {
-        alert("0~60사이 분을 입력해라~ ex)40분이면 40을 입력")
+}
+
+hoursMenu();
+
+function minMenu() {
+
+    var select = document.getElementById('alarmmins');
+    var min = 59;
+
+    for (i = 0; i <= min; i++) {
+        select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+    }
+}
+
+minMenu();
+
+function secMenu() {
+
+    var select = document.getElementById('alarmsecs');
+    var sec = 59;
+
+    for (i = 0; i <= sec; i++) {
+        select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+    }
+}
+
+secMenu();
+
+
+function alarmSet() {
+
+    var hr = document.getElementById('alarmhrs');
+
+    var min = document.getElementById('alarmmins');
+
+    var sec = document.getElementById('alarmsecs');
+
+    var ap = document.getElementById('ampm');
+
+
+    var selectedHour = hr.options[hr.selectedIndex].value;
+    var selectedMin = min.options[min.selectedIndex].value;
+    var selectedSec = sec.options[sec.selectedIndex].value;
+    var selectedAP = ap.options[ap.selectedIndex].value;
+
+    var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
+    console.log('alarmTime:' + alarmTime);
+
+    document.getElementById('alarmhrs').disabled = true;
+    document.getElementById('alarmmins').disabled = true;
+    document.getElementById('alarmsecs').disabled = true;
+    document.getElementById('ampm').disabled = true;
+
+
+//when alarmtime is equal to currenttime then play a sound
+    var h2 = document.getElementById('clock');
+
+    /*function to calcutate the current time
+    then compare it to the alarmtime and play a sound when they are equal
+    */
+
+    setInterval(function () {
+
+        var date = new Date();
+
+        var hours = (12 - (date.getHours()));
+        // var hours = date.getHours();
+
+        var minutes = date.getMinutes();
+
+        var seconds = date.getSeconds();
+
+        var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+        //convert military time to standard time
+
+        if (hours < 0) {
+            hours = hours * -1;
+        } else if (hours == 0) {
+            hours = 12;
+        } else {
+            hours = hours;
         }
-    else {
-        alert("숫자만 입력해라~ ex)40분이면 40을 입력")
-    }
+
+        var currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
 
 
-});
-// 분과 초를 정의합니다. math.floor을 사용하는 이유는 소숫점 형태로 나오기 때문에 정수로 표현하기 위해 사용합니다.
-function updateTime() {
-    const minutes = Math.floor(remainingSeconds / 60);
-    const seconds = remainingSeconds % 60;
-// 분과 초를 어떻게 화면에 표시할지 정합니다. textcontent는 모든 텍스트를 그대로 가져올 수 있습니다.
-    //padstart(HTML에 표시할 문자개수, 표시형식)
-    //toString은 분과 초를 문자로 바꿔 가져옵니다.
-    el.minutes.textContent = minutes.toString().padStart(2, "0");
-    el.seconds.textContent = seconds.toString().padStart(2, "0");
-}
-
-// 버튼을 사용할 때 일시정지 기능을 사용하는 효과를 줍니다. interval이 null 값일 때 play버튼이 보이도록하고
-// null 값이 아닐 때, 즉 일시정지 상태일 때 pause버튼이 보이도록 합니다. (시작하고 나서 play 버튼이 사라지는 문제가 발생하여 수정 중)
-// innerHTML은 요소 내 포함된 HTML 부분을 가져올 수 있습니다.
-function updateControls() {
-    if(interval === null) {
-        el.control.innerHTML = `<span class='material-icons'>play_arrow</span>`;
-        el.control.classList.add("timer_btn_start");
-        el.control.classList.remove("timer_btn_stop");
-    } else {
-        el.control.innerHTML = `<span class='material-icons'>pause</span>`;
-        el.control.classList.add("timer_btn_stop");
-        el.control.classList.remove("timer_btn_start");
-
-    }
-}
-// 타이머를 시작할 때의 함수를 정의합니다.
-function start() {
-    if (remainingSeconds === 0) return;
-    //setInterval 함수를 사용합니다. 남은 시간을 계속 줄이고 0이 되면 멈추게 합니다. 이를 interval 변수에 할당합니다.
-    interval = setInterval(() => {
-        remainingSeconds--;
-        updateTime();
-
-        if (remainingSeconds === 0) {
-            stop();
+        if (alarmTime == currentTime) {
+            showImage()
+            showAlert()
+            able_button()
+            sound.play();
         }
-    }, 1000); // timeout 이 1000일 때는 1초 10000일 때는 10초의 지연시간이 있습니다.
 
-updateControls();
+    }, 1000);
+
+
+    // console.log('currentTime:' + currentTime);
+
 }
 
-function stop() {
-    clearInterval(interval);
-    interval = null;
-    updateControls();
-    if (el.seconds <= 0 && el.minutes == 0)
-        showAlert()
-        showImage()
+
+function alarmClear() {
+
+    document.getElementById('alarmhrs').disabled = false;
+    document.getElementById('alarmmins').disabled = false;
+    document.getElementById('alarmsecs').disabled = false;
+    document.getElementById('ampm').disabled = false;
 }
