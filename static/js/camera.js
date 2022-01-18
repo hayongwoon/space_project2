b7528ab3a244d831724f1ecdcb1f947fed76fb92
winkberry <bellyamond@gmail.com>
2022-01-18 14:27:36 +0900

    camera js 수정

--- static/js/camera.js
+++ static/js/camera.js
@@ -1,9 +1,9 @@
-//카메라 모달창
-
+//카메라 모달창 구현
+//모달창 열기 함수 - 히든 클래스를 삭제해준다.
   const open = () => {
     document.querySelector(".camera-modal").classList.remove("hidden");
   }
-
+  //모달창 닫기 함수 - 히든 클래스를 활성화
   const close = () => {
     document.querySelector(".camera-modal").classList.add("hidden");
   }
@@ -13,15 +13,24 @@
   document.querySelector(".bg").addEventListener("click", close);
 
 
-//카메라 기능
+//비디오 재생
 const vid = document.querySelector('video');
+// 미디어 입력 장치 사용 권한을 요청. 사용자가 수락하면 mediastream을 반환한다.
+// 스트림은 카메라, 비디오 녹화장치, 스크린 공유 장치 등 하드웨어와 비디오 트랙, 마이크, 오디오 스트림 등이 있다.
+// navigator.mediaDevices.getUserMedia(constraints)
+// constraints 에는 오디오와 비디오 둘중 하나를 지정해야 한다. { audio: true, video: true }
+// video: { width: 1280, height: 720 } 비디오의 해상도를 지정할 수도 있다.
 navigator.mediaDevices.getUserMedia({video: true}) // request cam
 .then(stream => {
+  //HTMLMediaElement.srcObject : 미디어의 소스 역할을 하는 객체를 설정하거나 반환
+  //getUserMedia에서 반환된 stream을 video에 넣어 준다.
   vid.srcObject = stream; // don't use createObjectURL(MediaStream)
-  return vid.play(); // returns a Promise
+  return vid.play(); // returns a Promise 프로미스 반환. 미디어 재생
 })
+//사진 찍기 버튼 구현
 .then(()=>{ // enable the button
   const btn = document.querySelector('.snapshot');
+  //disabled 된 element는 사용할 수 없고, 클릭 할 수 없다.
   btn.disabled = false;
   btn.onclick = e => {
     takeASnap()
@@ -30,29 +39,31 @@ navigator.mediaDevices.getUserMedia({video: true}) // request cam
 })
 .catch(e=>console.log('please use the fiddle instead'));
 
+//사진 찍기 함수
 function takeASnap(){
-  const canvas = document.createElement('canvas'); // create a canvas
+  // canvas : 그래픽을 그리기위한 수단을 제공한다. 주로 2D 그래픽에 중점을 두고 있다.
+  const canvas = document.createElement('canvas'); // 캔버스 생성
   const ctx = canvas.getContext('2d'); // get its context
   canvas.width = vid.videoWidth; // set its size to the one of the video
-  canvas.height = vid.videoHeight;
+  canvas.height = vid.videoHeight; //캔버스의 너비와 높이를 비디오의 너비, 높이와 똑같이 한다.
   ctx.drawImage(vid, 0,0); // the video
   return new Promise((res, rej)=>{
+    //프로미스가 이행되면(res) 저장할 수 있는 파일이 반환 된다.
     canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
   });
 }
+//사진 저장 함수
 function download(blob){
   // uses the <a download> to download a Blob
-  let a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = 'screenshot.jpg';
-  document.body.appendChild(a);
-  a.click();
+  let a = document.createElement('a'); // a 엘리먼트를 생성한다.
+  // 주어진 객체를 가리키는 URL을 DOMString으로 반환
+  a.href = URL.createObjectURL(blob); //a의 주소를 정해준다?
+  a.download = 'screenshot.jpg'; //다운로드 파일 이름 지정
+  document.body.appendChild(a); //바디에 a를 추가해 준다.
+  // appendChild : 특정 부모 노드의 자식 노드 리스트 중 마지막 자식으로 붙
+  a.click(); //a 클릭 이벤트 실행
 }
 
-function sleep(ms) {
-  const wakeUpTime = Date.now() + ms;
-  while (Date.now() < wakeUpTime) {}
-}
 
 function camera_timer() {
     let timer_input = document.querySelector('.camera-timer-input').value
@@ -61,9 +72,10 @@ function camera_timer() {
     // let minus_num = timer_input / 1000
     // let ct_check = document.getElementById('ct-check')
 
+  // setInterval 함수는 함수를 반복적으로 부를 때마다 시간 딜레이를 두고 실행한다.
     const timer = setInterval(function () {
         display.textContent = String(timer_input)
-        if(--timer_input<0){
+        if(--timer_input<0){ //--가 무슨 뜻일까?
           takeASnap().then(download);
           display.textContent = '찰칵'
           clearInterval(timer)
@@ -82,31 +94,3 @@ function camera_timer() {
 }
 
 
-//
-// function startCountDown(duration, element) {
-//     let sec = duration;
-//     let countInterval = setInterval(function () {
-//
-//         second = parseInt(sec);
-//
-//         element.textContent = second;
-//
-//         second = second - 1;
-//         if (second < 0) { clearInterval(countInterval) };
-//
-//     }, 1000);
-// }
-//
-//
-// function camera_timer() {
-//     window.onload = function () {
-//         let second = document.querySelector('.camera-timer-input').value
-//
-//         let duration = second
-//         element = document.getElementById('ct-check')
-//         element.textContent = second;
-//
-//         setTimeout(function(){takeASnap().then(download);}, timer_input)
-//         startCountDown(--duration, element);
-//     }
-// }
